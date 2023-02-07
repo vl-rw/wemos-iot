5b42fa86d67d45d4d6fac2ce9c23c1d91f305bed
vl-rw <124644489+vl-rw@users.noreply.github.com>
2023-02-07 12:15:53 +1000

    Update server.py

--- gtk_application/server.py
+++ gtk_application/server.py
@@ -5,7 +5,7 @@ PORT = 1200
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
 
